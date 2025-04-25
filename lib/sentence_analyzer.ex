7a63755c00c7558bc5d42961d1dc6a98530f5084
Joshua <joshfouch777@gmail.com>
2025-04-25 12:10:01 -0400

    slight chnages

--- lib/sentence_analyzer.ex
+++ lib/sentence_analyzer.ex
@@ -2,9 +2,9 @@ defmodule SentenceAnalyzer do
   use Application
 
   # my input
-  @input "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.
-We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. That to secure these rights, Governments are instituted among Men, deriving their self-evident just powers from the consent of the governed, That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world."
-  #@input "Flappy Bird is my favorite, game. How about your favorite game?"
+  #@input "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.
+#We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. That to secure these rights, Governments are instituted among Men, deriving their self-evident just powers from the consent of the governed, That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world."
+  @input "It is better to be with God and be a beggar than to be with the devil and be a king. But who among us is brave enough to beg?"
 
   # for starting the program
   def start(_type, _args) do
@@ -14,7 +14,7 @@ We hold these truths to be self-evident, that all men are created equal, that th
 
   end
 
-  # for reading it the input and outputting results of functions
+  # for reading the input and outputting results of functions
   def analyze(input) do
 
     # word count
@@ -40,10 +40,9 @@ We hold these truths to be self-evident, that all men are created equal, that th
   # function outputs list of words, uncapitalized, and no other symbols
   def word_list(input) do
 
-    # split function with regex delimiter, pipe into length()
     # uncapitalize everything for frequency count
     input
-    |> String.split(~r/[\s]+/, trim: true)
+    |> String.split() # defaults to splitting by whitespace
     |> Enum.map(&String.downcase/1) # map HOF!
     |> Enum.map(&delete_symbols/1)
 
@@ -93,7 +92,6 @@ We hold these truths to be self-evident, that all men are created equal, that th
     word_list(words)
     |> freqy(%{})
     |> filter_repeated()
-    |> Enum.into(%{})
 
   end
 
@@ -105,7 +103,7 @@ We hold these truths to be self-evident, that all men are created equal, that th
   # map for repeated word frequency
   def freqy([head | rest], freqmap) do
 
-    # if exist, incremment counter, if not exist, add the map
+    # if exist, incremment counter, if not exist, add 1 to the map
     freqymap = Map.update(freqmap, head, 1, &(&1 + 1))
 
     # Map.update(map, key, default, function)
@@ -122,7 +120,7 @@ We hold these truths to be self-evident, that all men are created equal, that th
   def filter_repeated(map) do
 
     map
-    |> Enum.filter(fn {_, acc} -> acc > 1 end)  # HOF filter!
+    |> Enum.filter(fn {_, val} -> val > 1 end)  # HOF filter!
 
     # Enum.filter(map, fn element -> condition end)
 
